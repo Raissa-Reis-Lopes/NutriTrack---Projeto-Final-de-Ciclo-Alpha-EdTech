d32898fe4fc49c6a3aab110ce263fff58f9efd61
Raissa Reis Lopes <raissa.vilemon@gmail.com>
2024-04-09 02:56:47 +0000

    install dotenv, test server on port 3000, create file .env for example

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
 
 require('dotenv').config();
+
 const port = process.env.PORT || 3000;
-const dbHost = process.env.DB_HOST;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
+
+//Vou manter a estrutura que usaremos qnd colocar o banco de dados mas deixar comentado enquanto não usamos
+// const dbHost = process.env.DB_HOST;
+// const dbUser = process.env.DB_USER;
+// const dbPassword = process.env.DB_PASSWORD;
+
+app.get('/',(req, res) =>{
+    res.send('Olá, mundo!');
+})
+
+app.listen(port, ()=>{
+    console.log(`Server running on http://localhost:${port}`);
+})
+
