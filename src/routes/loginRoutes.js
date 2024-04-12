d9bf8f3e8dd7fb3f4c73d1372e8c31b92a7ddd51
Raissa Reis Lopes <raissa.vilemon@gmail.com>
2024-04-12 03:11:07 +0000

    Create loginController

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const loginController = require('../controllers/loginController.js');
 
-router.post('/', loginController.autenticate);
+router.post('/', loginController.authenticate);
 
 module.exports = router;
 
