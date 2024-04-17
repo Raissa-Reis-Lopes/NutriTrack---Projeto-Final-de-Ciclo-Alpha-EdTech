67ea016b3419cb751cfa36babee8abbae355f740
Raissa Reis Lopes <raissa.vilemon@gmail.com>
2024-04-17 05:34:37 +0000

    Delete meal files to update to food_added

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ const userRoutes = require('./userRoutes');
 const loginRoutes = require('./loginRoutes');
 const foodPlanRoutes = require('./foodPlanRoutes');
 const foodRoutes = require('./foodRoutes');
-const mealRoutes = require('./mealRoutes')
 const configHistoryRoutes = require('./configHistoryRoutes');
 const calculateRoutes = require('./calculateRoutes')
 
@@ -14,7 +13,6 @@ router.use('/users', userRoutes);          //checked on postman
 router.use('/login', loginRoutes);         //checked on postman
 router.use('/foodPlan', foodPlanRoutes);  //checked on postman
 router.use('/food', foodRoutes);          //checked on postman 
-router.use('/meal', mealRoutes);          //checked on postman  
 router.use('/config', configHistoryRoutes); //checked on postman
 router.use('/calculate', calculateRoutes);
 
