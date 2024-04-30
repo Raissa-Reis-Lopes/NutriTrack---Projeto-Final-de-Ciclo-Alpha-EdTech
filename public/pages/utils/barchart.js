650dc6fbc5b33fe03379e8f3dc8a61d94ef65464
Raissa Reis Lopes <raissa.vilemon@gmail.com>
2024-04-30 20:48:30 -0300

    Fix bar chart to the correct local day

--- public/pages/utils/barchart.js
+++ public/pages/utils/barchart.js
@@ -8,69 +8,6 @@ export function renderBarChart(responseData, container, colorCalorie, colorProte
   console.log(responseData)
   console.log(responseData.data)
 
-
-  // Preparar os dados para o gráfico
-//   const chartData = {
-//       labels: daysOfWeek,
-//       datasets: [
-//           {
-//               label: 'Calorias',
-//               backgroundColor: colorCalorie,
-//               data: daysOfWeek.map(day => responseData.data[day] ? Math.ceil(responseData.data[day].totalNutrition.calories) : 0)
-//           },
-//           {
-//               label: 'Proteína',
-//               backgroundColor: colorProtein,
-//               data: daysOfWeek.map(day => responseData.data[day] ? Math.ceil(responseData.data[day].totalNutrition.protein) : 0)
-//           },
-//           {
-//               label: 'Carboidrato',
-//               backgroundColor: colorCarbo,
-//               data: daysOfWeek.map(day => responseData.data[day] ? Math.ceil(responseData.data[day].totalNutrition.carbohydrate) : 0)
-//           },
-//           {
-//               label: 'Gordura',
-//               backgroundColor: colorLipid,
-//               data: daysOfWeek.map(day => responseData.data[day] ? Math.ceil(responseData.data[day].totalNutrition.lipid) : 0)
-//           }
-//       ]
-//   };
-
-//   const chartOptions = {
-//     responsive: true,
-//     scales: {
-//         y: {
-//             beginAtZero: true,
-//             title: {
-//                 display: true,
-//                 text: 'Calorias',
-//             },
-//             position: 'left', // Posição do eixo y para as calorias
-//         },
-//         y1: {
-//             beginAtZero: true,
-//             title: {
-//                 display: true,
-//                 text: 'Proteína, Gordura e Carboidrato',
-//             },
-//             position: 'right', // Posição do segundo eixo y
-//             grid: {
-//                 drawOnChartArea: false, // Não desenhar a grade no espaço do gráfico
-//             },
-//         },
-//         x: {
-//             stacked: false,
-//             title: {
-//                 display: true,
-//                 text: 'Dias da Semana',
-//                 ticks: {
-//                   color: 'rgba(0, 0, 0, 0.8)' // Definir a cor dos rótulos
-//               }
-//             }
-//         }
-//     }
-// };
-
 const chartOptions = {
     responsive: true,
     scales: {
@@ -84,7 +21,7 @@ const chartOptions = {
             },
             // Define o range do eixo y para calorias
             min: 0,
-            max: 3000, // Defina o valor máximo conforme necessário
+            // max: 3000, // Defina o valor máximo conforme necessário
         },
         y1: {
             type: 'linear',
@@ -94,9 +31,8 @@ const chartOptions = {
                 display: true,
                 text: 'Proteína, Gordura e Carboidrato',
             },
-            // Define o range do eixo y1 para proteína, gordura e carboidrato
             min: 0,
-            max: 200, // Defina o valor máximo conforme necessário
+            // max: 200, 
             grid: {
                 drawOnChartArea: false,
             },
