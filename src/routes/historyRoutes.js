25d96ecb09defca642e1c5b6f19cc59bcd4537e8
Luiz Alves <luizalvestreze@gmail.com>
2024-04-30 17:00:38 -0400

    Exclude not used archives

--- src/routes/historyRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const historyController = require('../controllers/historyController');
-
-router.get('/week', historyController.weekData);
-
-module.exports = router;
