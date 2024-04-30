8473de76eed9d1dc2098a78dbcf5eb2829406da6
Raissa Reis Lopes <raissa.vilemon@gmail.com>
2024-04-30 04:22:20 -0300

    WIP - create barchart on history

--- public/pages/modules/history.js
+++ public/pages/modules/history.js
@@ -1,9 +1,10 @@
 import createCustomEvent from "./event.js";
 // import { limitDate } from "../utils/limitDates.js";
 import { logout } from "../utils/logout.js";
-import { generateBarChart } from '../utils/barchart.js';
+// import { generateBarChart } from '../utils/barchart.js';
 import { privacyPolicyModal, termsModal, sacModal, createModalEventsDefault } from "./modals.js";
 import { footerHistory } from "./footer.js";
+import { renderBarChart } from "../utils/barchart.js";
 
 export function History() {
     const div = document.createElement("div");
@@ -14,6 +15,9 @@ export function History() {
         <div class="logo" id="logo">
             <img src="./img/logo.svg" alt="NutriTrack">
         </div>
+        <div class="titles">
+        <h1>Histórico</h1>
+    </div>
         <nav class="header_nav">
             <div id="navHome">Home</div>
             <div id="navProfile">Perfil</div>
@@ -21,26 +25,19 @@ export function History() {
         </nav>
     </header>
     <main class="history_container">
-        <div class="titles">
-            <h1>Histórico</h1>
-            <h5>Calorias consumidas:</h5>
-            <h5>Meta de calorias:</h5>
-        </div>
-        <div class="period">
-            <div id="input-date">
-                <button class="btn_stroke" onclick="navegar('-')">← Anterior</button>
-                <input type="date" id="dataInicio">
-                <input type="date" id="dataFim">
-                <button class="btn_stroke" onclick="navegar('+')">Próximo →</button>
-            </div>
-        </div>
-        <div class="chart">
+       
+        <div id="dateDisplay"></div>
+        <div class="chart_history">
         <section>
             <div class="chart-container">
                 <span class="span_green ">Gráfico Semanal</span>
                 <div>
-                    <div class="chart" id="week-chart"></div>
+                  <canvas class="chart_canvas" id="week-chart"></canvas>
                 </div>
+            </div>
+            <div class="align_row"> 
+            <button id="previousWeekBtn" class="btn_history">Semana Anterior</button>
+            <button id="nextWeekBtn" class="btn_history">Próxima Semana</button>
             </div>
         </section>
         </div>
@@ -55,39 +52,6 @@ export function History() {
     document.getElementById("root").innerHTML = '';
     document.getElementById("root").appendChild(div);
     // limitDate('input-date');
-
-    const dataAtual = new Date();
-    const dataInicioInput = document.getElementById('dataInicio');
-    const dataFimInput = document.getElementById('dataFim');
-
-    // Define a data atual como o valor inicial dos elementos de entrada de data
-    dataInicioInput.value = formatarData(dataAtual);
-    dataFimInput.value = formatarData(new Date(dataAtual.getTime() - (7 * 24 * 60 * 60 * 1000))); // Adiciona uma semana à data atual
-
-    const inputDate = document.getElementById('input-date');
-  
-    inputDate.addEventListener("change", function() {
-        const selectedDate = inputDate.value;
-        // getweek(selectedDate);
-        // loadUserDataForDate(selectedDate); // Carrega os dados para a data selecionada
-        
-        let dataInicio = new Date(document.getElementById('dataInicio').value);
-        let dataFim = new Date(document.getElementById('dataFim').value);
-        let dias = calcularDias(dataInicio, dataFim);
-        
-        // Define dataInicio para o último domingo
-        dataInicio.setDate(dataInicio.getDate() - dataInicio.getDay());
-        document.getElementById('dataInicio').value = formatarData(dataInicio);
-        
-        // Define dataFim para o próximo sábado
-        dataFim.setDate(dataInicio.getDate() + 6);
-        document.getElementById('dataFim').value = formatarData(dataFim);
-    });
-
-    const dias = [];
-    generateBarChart(dias);
-
-
 
      // Para os modais: Pegar a seção onde ele fica, chamar a função para obter o modal, adicionar ao container:
      const privacyModalContainer = document.getElementById('privacy_policy_container');
@@ -108,63 +72,222 @@ export function History() {
      
      //OBSERVAÇÃO, ESSE FUNCÃO TEM QUE VIR SÓ DEPOIS QUE PEGAR TODOS OS MODAIS
      createModalEventsDefault();
-
+    getDateAndRenderChart();
      navRoutes();
+
+     // Dados falsos para simular um conjunto de dados de nutrição
+const fakeResponseData = {
+    success: true,
+    data: {
+        "2024-04-25": {
+            totalNutrition: {
+                calories: 200,
+                protein: 20,
+                carbohydrate: 30,
+                lipid: 10
+            }
+        },
+        "2024-04-26": {
+            totalNutrition: {
+                calories: 250,
+                protein: 25,
+                carbohydrate: 35,
+                lipid: 12
+            }
+        },
+        "2024-04-27": {
+            totalNutrition: {
+                calories: 300,
+                protein: 30,
+                carbohydrate: 40,
+                lipid: 15
+            }
+        },
+    }
+};
+
+const chartContainer = document.getElementById('week-chart');
+// Agora você pode chamar sua função renderBarChart com os dados falsos
+renderBarChart(fakeResponseData, chartContainer);
+
     
     return div
 }
 
-function formatarData(data) {
-    const ano = data.getFullYear();
-    const mes = String(data.getMonth() + 1).padStart(2, '0'); // +1 porque janeiro é 0
-    const dia = String(data.getDate()).padStart(2, '0');
-    
-    return `${ano}-${mes}-${dia}`;
-}
 
-function navegar(direcao) {
-    if (direcao === '-') {
-        dataInicio.setDate(dataInicio.getDate() - 7);
-        dataFim.setDate(dataFim.getDate() - 7);
-    } else if (direcao === '+') {
-        dataInicio.setDate(dataInicio.getDate() + 7);
-        dataFim.setDate(dataFim.getDate() + 7);
+async function getUserId(){
+    try {
+        const getUserId = await fetch("/api/login/", {
+            method: "GET",
+        });
+
+        if(!getUserId.ok){
+            throw new Error("Falha ao localizar o id do usuáiro")
+        }
+        const userIdResponse = await getUserId.json();
+        const userId = userIdResponse.user;
+        return userId;
+
+    } catch (error) {
+        
     }
-
-    document.getElementById('dataInicio').value = formatarData(dataInicio);
-    document.getElementById('dataFim').value = formatarData(dataFim);
 }
 
-// function calcularDias(dataInicio, dataFim) {
-//     const dataAtual = new Date(dataInicio);
 
-//     while (dataAtual <= dataFim) {
-//         let dia = {
-//             data: formatarDataGrafico(dataAtual, 'ddd'),
-//             calorias: Math.floor(Math.random() * 1000),
-//             gordura: Math.floor(Math.random() * 100),
-//             proteina: Math.floor(Math.random() * 100),
-//             carboidrato: Math.floor(Math.random() * 200)
-//         };
-//         dias.push(dia);
-//         dataAtual.setDate(dataAtual.getDate() + 1);
+async function fetchChartData(startDate, endDate){
+
+   const userId = await getUserId();
+   try {
+    const response = await fetch(`/api/foodAdded/periodNutrition?user_id=${userId}&start_date=${startDate}&end_date=${endDate}`);
+    const responseData = await response.json();
+    console.log(responseData)
+
+    console.log(`Esse é o responseData na função fetchCharData: ${responseData}`)
+    return responseData;
+   } catch (error) {
+        console.log(error)
+   }
+};
+
+function formatDate(date){
+    const day = String(date.getDate()).padStart(2, '0');
+         const month = String(date.getMonth() + 1).padStart(2, '0');
+         const year = date.getFullYear();
+         return `${day}/${month}/${year}`;
+}
+
+function getWeekRange(date){
+    const dayOfWeek = date.getDay(); // Dia da semana (0 = domingo, 1 = segunda, ..., 6 = sábado)
+    const firstDay = new Date(date); // Clonar a data fornecida
+    firstDay.setDate(date.getDate() - dayOfWeek); // Definir a data para o domingo da semana
+    const lastDay = new Date(firstDay); // Clonar a data do primeiro dia da semana
+    lastDay.setDate(firstDay.getDate() + 6); // Definir a data para o sábado da semana
+    return [firstDay, lastDay];
+}
+
+async function getDateAndRenderChart(){
+     // Elementos do DOM
+     const dateDisplay = document.getElementById('dateDisplay');
+     const previousWeekBtn = document.getElementById('previousWeekBtn');
+     const nextWeekBtn = document.getElementById('nextWeekBtn');
+     const chartContainer = document.getElementById('week-chart');
+
+      // Função para converter o formato da data de DD/MM/YYYY para YYYY-MM-DD
+      const convertDateFormat = (date) => {
+        console.log(`Data no formatDate ${date}`)
+        const [day, month, year] = date.split('/');
+        return `${year}-${month}-${day}`;
+    }
+ 
+     // Data atual
+     const today = new Date();
+ 
+     // Função para atualizar a exibição da data com base na data atual
+     async function updateDateDisplay(date){
+         const [startOfWeek, endOfWeek] = getWeekRange(date);
+         const startDate = formatDate(startOfWeek);
+         const endDate = formatDate(endOfWeek);
+         dateDisplay.textContent = `Período: ${startDate} - ${endDate}`;
+         const responseData = await fetchChartData(convertDateFormat(startDate), convertDateFormat(endDate));
+         console.log(`Esse é o responseData na função updateDateDisplay: ${responseData}`)
+         renderBarChart(responseData, chartContainer)
+         return { startDate, endDate };
+     };
+ 
+     // Atualizar a exibição da data com a data atual
+     updateDateDisplay(today);
+ 
+     // Evento de clique no botão de semana anterior
+     previousWeekBtn.addEventListener('click', async function() {
+         today.setDate(today.getDate() - 7); // Retroceder 7 dias (uma semana)
+         const { startDate, endDate } = updateDateDisplay(today); // Atualizar a exibição da data e obter as datas de início e fim do período
+         const responseData = await fetchChartData(convertDateFormat(startDate), convertDateFormat(endDate));
+         console.log(`Esse é o responseData na função previousWeekBtn: ${responseData}`)
+         renderBarChart(responseData, chartContainer)
+     });
+ 
+   // Evento de clique no botão de próxima semana
+     nextWeekBtn.addEventListener('click', async function() {
+         today.setDate(today.getDate() + 7); // Avançar 7 dias (uma semana)
+         const { startDate, endDate } = updateDateDisplay(today); // Atualizar a exibição da data e obter as datas de início e fim do período
+         console.log(startDate);
+         console.log(endDate)
+         const responseData = await fetchChartData(convertDateFormat(startDate), convertDateFormat(endDate));
+         console.log(`Esse é o responseData na função nextWeekBtn: ${responseData}`)
+         renderBarChart(responseData, chartContainer)
+     });
+ 
+    
+}
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Elementos do DOM
+//     const dateDisplay = document.getElementById('dateDisplay');
+//     const previousWeekBtn = document.getElementById('previousWeekBtn');
+//     const nextWeekBtn = document.getElementById('nextWeekBtn');
+
+
+//     // Data atual
+//     const today = new Date();
+
+//     // Função para formatar a data como 'DD/MM/YYYY'
+//     const formatDate = (date) => {
+//         const day = String(date.getDate()).padStart(2, '0');
+//         const month = String(date.getMonth() + 1).padStart(2, '0');
+//         const year = date.getFullYear();
+//         return `${day}/${month}/${year}`;
+//     };
+
+//     // Função para obter o primeiro e o último dia da semana com base na data fornecida (domingo como primeiro dia da semana)
+//     const getWeekRange = (date) => {
+//         const dayOfWeek = date.getDay(); // Dia da semana (0 = domingo, 1 = segunda, ..., 6 = sábado)
+//         const firstDay = new Date(date); // Clonar a data fornecida
+//         firstDay.setDate(date.getDate() - dayOfWeek); // Definir a data para o domingo da semana
+//         const lastDay = new Date(firstDay); // Clonar a data do primeiro dia da semana
+//         lastDay.setDate(firstDay.getDate() + 6); // Definir a data para o sábado da semana
+//         return [firstDay, lastDay];
+//     };
+
+//     // Função para atualizar a exibição da data com base na data atual
+//     const updateDateDisplay = (date) => {
+//         const [startOfWeek, endOfWeek] = getWeekRange(date);
+//         const startDate = formatDate(startOfWeek);
+//         const endDate = formatDate(endOfWeek);
+//         dateDisplay.textContent = `Período: ${startDate} - ${endDate}`;
+//         return { startDate, endDate };
+//     };
+
+//     // Atualizar a exibição da data com a data atual
+//     updateDateDisplay(today);
+
+//     // Evento de clique no botão de semana anterior
+//     previousWeekBtn.addEventListener('click', async function() {
+//         today.setDate(today.getDate() - 7); // Retroceder 7 dias (uma semana)
+//         const { startDate, endDate } = updateDateDisplay(today); // Atualizar a exibição da data e obter as datas de início e fim do período
+//         await fetchDataAndRenderChart(userId, convertDateFormat(startDate), convertDateFormat(endDate)); // Fazer a requisição ao backend e renderizar o gráfico
+//     });
+
+//   // Evento de clique no botão de próxima semana
+//     nextWeekBtn.addEventListener('click', async function() {
+//         today.setDate(today.getDate() + 7); // Avançar 7 dias (uma semana)
+//         const { startDate, endDate } = updateDateDisplay(today); // Atualizar a exibição da data e obter as datas de início e fim do período
+//         await fetchDataAndRenderChart(userId, convertDateFormat(startDate), convertDateFormat(endDate)); // Fazer a requisição ao backend e renderizar o gráfico
+//     });
+
+//     // Função para converter o formato da data de DD/MM/YYYY para YYYY-MM-DD
+//     const convertDateFormat = (date) => {
+//         const [day, month, year] = date.split('/');
+//         return `${year}-${month}-${day}`;
 //     }
+// });
 
-//     return dias;
-// }
 
-function formatarDataGrafico(data, formato) {
-    let diaSemana = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
-    let ano = data.getFullYear();
-    let mes = String(data.getMonth() + 1).padStart(2, '0'); // +1 porque janeiro é 0
-    let dia = String(data.getDate()).padStart(2, '0');
-    
-    if (formato === 'ddd') {
-        return `${diaSemana[data.getDay()]} ${dia}/${mes}`;
-    }
+//Cada vez que clicar no botão de anterior ou próximo ele refaz a requisição e o gráfico
 
-    return `${ano}-${mes}-${dia}`;
-}
+
+
+
+
 
 export function navRoutes() {
     const navProfile = document.getElementById("navProfile");
