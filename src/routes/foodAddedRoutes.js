aa11d37de2c90ec9395b982601131f421b4a36ae
Raissa Reis Lopes <raissa.vilemon@gmail.com>
2024-04-30 00:06:55 -0300

    Create new route to get the total nutrition for a period

--- src/routes/foodAddedRoutes.js
+++ src/routes/foodAddedRoutes.js
@@ -9,6 +9,8 @@ router.get('/', foodAddedController.getAllFoodsAdded);
 router.get('/byUserId/:id', foodAddedController.getFoodsAddedByUserId);
 //Com query /dailyConsumedWithDetail?user_id= &date=
 router.get('/dailyConsumedWithDetail', foodAddedController.calculateDailyNutritionWithDetails);
+//Com query /totalNutritionInPeriod?user_id= &start_date= &end_date=
+router.get('/periodNutrition', foodAddedController.calculatePeriodNutritionSummary)
 router.get('/:id', foodAddedController.getFoodAddedById);
 
 
