4924e19fa87ba3a2b2987d59bdc0466373dc52be
Gabriela <gabisgamba@gmail.com>
2024-05-02 21:42:08 -0300

    correcting the text all right reserved

--- public/pages/modules/footer.js
+++ public/pages/modules/footer.js
@@ -3,7 +3,7 @@ export function footerProfile(){
     sectionFooterProfile.classList.add("footer")
     sectionFooterProfile.classList.add("footer_profile")
     sectionFooterProfile.innerHTML=`
-        <span>all rights reserved</span>
+        <span>todos os direitos reservados</span>
         <span id="open-modal-terms">Termos de uso</span>
         <span id="open-modal-privacy">Politica de Privacidade</span>
         <div >
@@ -19,7 +19,7 @@ export function footerHome(){
     sectionFooterHome.classList.add("footer")
     sectionFooterHome.classList.add("footer_home")
     sectionFooterHome.innerHTML=`
-        <span>all rights reserved</span>
+        <span>todos os direitos reservados</span>
         <span id="open-modal-terms">Termos de uso</span>
         <span id="open-modal-privacy">Politica de Privacidade</span>
         <div >
@@ -35,7 +35,7 @@ export function footerHistory(){
     sectionFooterHistory.classList.add("footer")
     sectionFooterHistory.classList.add("footer_history")
     sectionFooterHistory.innerHTML=`
-        <span>all rights reserved</span>
+        <span>todos os direitos reservados</span>
         <span id="open-modal-terms">Termos de uso</span>
         <span id="open-modal-privacy">Politica de Privacidade</span>
         <div >
